861426749314b8e891ef21a13b215dfe0ff84552
Soulstorm <teddy.cozette@hotmail.fr>
2024-10-09 20:28:37 +0200

    Fonction pour enregistrer uun menu principal

--- NathalieMota/functions.php
+++ NathalieMota/functions.php
@@ -4,3 +4,8 @@
 add_theme_support( 'custom-logo' );
 
 // Function pour enregistrer un nouveau menu
+function register_custom_menu() {
+    register_nav_menu( 'main-menu', __( 'Menu Principal', 'theme-text-domain' ) );
+}
+
+add_action( 'after_setup_theme', 'register_custom_menu');
