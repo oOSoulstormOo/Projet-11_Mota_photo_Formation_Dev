eff8781a703dffdae462aa60c46821fd1a144f88
Soulstorm <teddy.cozette@hotmail.fr>
2024-10-17 13:10:40 +0200

    Ajout du menu custom pour le footer

--- NathalieMota/footer.php
+++ NathalieMota/footer.php
@@ -1,6 +1,14 @@
 
 
-      <footer>
+      <footer id='footer'>
+      <nav class='navigation-footer'>
+              <?php
+              wp_nav_menu( array(
+                'theme_location' => 'footer',
+                ));
+              ?>
+              <span>Tous droits réservés</span>
+            </nav>
        
       </footer>
     <?php wp_footer(); ?>
