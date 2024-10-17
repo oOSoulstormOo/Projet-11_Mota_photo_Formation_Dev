cc144fd7ecd5f3adea451e2b3a326eb1fc4c372d
Soulstorm <teddy.cozette@hotmail.fr>
2024-10-17 17:10:35 +0200

    Ajout du menu mobile, interface de navigation + Appel des police d'écriture Google Fonts

--- NathalieMota/header.php
+++ NathalieMota/header.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Nathalie Mota</title>
+      <!-- Appel des polices depuis le CDN de Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- Space Mono + Poppins -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;1,300;1,500&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
   </head>
   <body>
@@ -23,6 +28,24 @@
                 'theme_location' => 'main',
                 ));
               ?>
+            </nav>
+            <!-- Menu burger -->
+             <!-- Bouton -->
+            <button class="menu-burger" aria-controls="primary-menu" aria-expanded="false">
+                <span class="line line__1"></span>
+                <span class="line line__2"></span>
+                <span class="line line__3"></span>
+            </button>
+          </div>
+          <!-- Conteneur menu sur mobile -->
+          <div class="burger-open-menu">
+          <!-- Menu de navigation mobile -->
+            <nav class='navigation-mobile'>
+                <?php
+                wp_nav_menu( array(
+                  'theme_location' => 'main',
+                  ));
+                ?>
             </nav>
           </div>
         
