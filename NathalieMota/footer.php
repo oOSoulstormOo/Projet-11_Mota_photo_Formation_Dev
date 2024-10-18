fa68d81a0f10157b9adbbddacdee9d9ab6989490
Soulstorm <teddy.cozette@hotmail.fr>
2024-10-18 19:26:28 +0200

    Ajout de la modale

--- NathalieMota/footer.php
+++ NathalieMota/footer.php
@@ -11,6 +11,12 @@
             </nav>
        
       </footer>
+
+      <!-- Lance la popup contact -->
+	    <?php 
+        get_template_part ( 'template-parts/modal-contact'); 		
+      ?>
+
     <?php wp_footer(); ?>
   </body>
 </html>
