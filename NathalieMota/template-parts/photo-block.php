cb05c6e28d2b3a38dc7c0d759773c7c68be614a0
Soulstorm <teddy.cozette@hotmail.fr>
2024-10-29 20:28:47 +0100

    Fichier pouur faire appelle a un block photo qui change dynamiquement selon les info reçu

---
+++ NathalieMota/template-parts/photo-block.php
@@ -0,0 +1,22 @@
+<div class="photo-block">
+    <div class="photo-image">
+        <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($title); ?>">
+    </div>
+    <div class="photo-overlay">
+        <div class="overlay-content">
+            <a href="<?php echo esc_url(get_permalink($post_id)); ?>" class="overlay-link">
+                <img src="<?php echo get_template_directory_uri() . '/assets/image/Icon_eye.png' ;?> ">
+            </a>    
+         <div class="overlay-ref">
+            <?php echo esc_html($reference); ?>
+         </div>
+         <div class="overlay-cat">
+            <?php echo esc_html($categorieArticle); ?>
+         </div>
+         <div class="overlay-fullscreen">
+            <button><img src="<?php echo get_template_directory_uri() . '/assets/image/Icon_fullscreen.png' ;?>"></button>
+         </div>
+            
+        </div>
+    </div>
+</div>
